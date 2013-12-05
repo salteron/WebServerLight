260bee4974946d3dd78b4f6f4331525ebe71cb72
Michael Sogomonyan <sogomonyan@ek.apress.ru>
2013-12-05 12:06:01 +0600

    Изменяет способ хранения статистики : localmemcache

--- app/tools/stats_collector.rb
+++ app/tools/stats_collector.rb
@@ -1,13 +1,17 @@
 # -*- encoding : utf-8 -*-
 
+require 'localmemcache'
+
 module WebServerLight
   module Tools
     class StatsCollector
+      STATS_FILE_PATH = './viewcounters.lmc'
+
       class << self
         attr_accessor :stats
       end
 
-      @stats = {}
+      @stats = LocalMemCache.new filename: STATS_FILE_PATH
 
       def initialize
         @mutex = Mutex.new
@@ -17,9 +21,12 @@ module WebServerLight
         Thread.new do
           @mutex.synchronize do
             StatsCollector.stats[file_path] ||= 0
-            StatsCollector.stats[file_path] += 1
+            StatsCollector.stats[file_path] =
+              StatsCollector.stats[file_path].to_i + 1
           end
         end
+      rescue LocalMemCacheError => e
+        puts e.message.red
       end
     end
   end
